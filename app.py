143e9c45ea47a3c4d29b98e1dcbdcae4525460f4
vali1278 <valiullah1278@gmail.com>
2026-06-05 10:50:01 +0530

    Add admin route and corresponding template

--- app.py
+++ app.py
@@ -30,5 +30,9 @@ def daily_report():
 def photo_upload():
     return render_template('photo_upload.html')
 
+@app.route('/admin')
+def admin():
+    return render_template('admin_dashboard.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
