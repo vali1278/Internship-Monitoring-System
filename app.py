8424699522154ea18a7603b1a15b59fe0c93a794
vali1278 <valiullah1278@gmail.com>
2026-06-05 10:37:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
